6db10dc86d7a56c0c8ce9bcbcb825c0684117cda
khalidtouch <khalidhulabaloo9@gmail.com>
2023-08-08 17:19:19 +0100

    added a Github actions file

--- tests/user_test.go
+++ tests/user_test.go
@@ -8,7 +8,6 @@ import (
 
 
 func (t * SuiteTest) TestCreateUser() {
-	service.ClearAllUsers()
 	_, err := service.CreateUser(entity.User{
 		Name: "username1",
 		Email: "username1@gmail.com",
@@ -23,16 +22,28 @@ func (t * SuiteTest) TestCreateUser() {
 
 	_, err = service.CreateUser(entity.User{
 		Name: "username3",
-		Email: "username2@gmail.com",
+		Email: "username3@gmail.com",
 	})
-	assert.Error(t.T(), err)
-
+	assert.NoError(t.T(), err)
 	service.ClearAllUsers()
 }
 
 
 func (t *SuiteTest) TestGetAllUsers() {
-	_, err := service.GetAllUsers()
+	users, err := service.GetAllUsers()
 
 	assert.NoError(t.T(), err)
+	assert.Equal(t.T(), 0, len(users))
+
+	_, err = service.CreateUser(entity.User{
+		Name: "test_rat1",
+		Email: "test_rat@gmail.com",
+	})
+
+	assert.NoError(t.T(), err)
+
+	user, err := service.GetUserByEmail("test_rat@gmail.com")
+	assert.NoError(t.T(), err)
+	assert.Equal(t.T(), "test_rat@gmail.com", user.Email)
+	assert.Equal(t.T(), "test_rat1", user.Name)
 }
